6a664ec116ef00bbda000454089b43576c34306d
Keeko <787018661@qq.com>
2023-06-20 15:31:06 +0800

    0543二叉树的直径

---
+++ problems/0543二叉树的直径.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=javascript
+ * @lcpr version=21909
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+  let res = 0;
+  function getHeight(root) {
+    if (root === null) {
+      return 0;
+    }
+    const l = getHeight(root.left);
+    const r = getHeight(root.right);
+    res = Math.max(res, l + r);
+    return Math.max(l, r) + 1; // 返回当前树的树高
+  }
+  getHeight(root);
+  return res;
+};

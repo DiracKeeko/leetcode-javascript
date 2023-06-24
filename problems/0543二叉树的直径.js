dc67d4a09c799c96577cb35c53cc5b1f0fe392ba
Keeko <787018661@qq.com>
2023-06-24 10:59:37 +0800

    0222完全二叉树的节点个数

--- problems/0543二叉树的直径.js
+++ problems/0543二叉树的直径.js
@@ -18,6 +18,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// 二叉树的直径是指树中任意两个节点之间最长路径的长度。 这条路径可能经过也可能不经过根节点 root。
+// 两节点之间路径的长度由它们之间边数表示。
 var diameterOfBinaryTree = function(root) {
   let res = 0;
   function getHeight(root) {

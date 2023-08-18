ada62a8d888323041df30ce5363f7644936990c3
keeko <787018661@qq.com>
2023-08-18 23:32:56 +0800

    0104二叉树的最大深度 review

--- problems/0104二叉树的最大深度.js
+++ problems/0104二叉树的最大深度.js
@@ -22,11 +22,10 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  function getHeight(root) {
-    if (root === null) {
-      return 0;
-    }
-    return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+  // ↓ 终止条件
+  if (root === null) {
+    return 0;
   }
-  return getHeight(root);
+  // ↓ 树的最大深度 = 左子树的深度 和 右子树深度 最大的那个 + 1
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };

776468587699eefa267ab1ded999cc104de9a173
Keeko <787018661@qq.com>
2023-09-05 17:29:49 +0800

    0322零钱兑换 逻辑梳理

--- problems/0322零钱兑换.js
+++ problems/0322零钱兑换.js
@@ -20,10 +20,9 @@ var coinChange = function (coins, amount) {
   dp[0] = 0;
   for (let i = 0; i <= amount; i++) {
     for (let coin of coins) {
-      if (i < coin) {
-        continue;
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
   return dp[amount] === Infinity ? -1 : dp[amount];

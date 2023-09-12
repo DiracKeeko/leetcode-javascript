e199614b1bcb5092e112df8aa5e6725f17e6b5fb
keeko <787018661@qq.com>
2023-09-13 04:08:35 +0800

    0876链表的中间结点

--- problems/0021合并两个有序链表.js
+++ problems/0021合并两个有序链表.js
@@ -39,3 +39,21 @@ var mergeTwoLists = function(list1, list2) {
   }
   return dummy.next;
 };
+
+// 应该用下面这种
+var mergeTwoLists2 = function (list1, list2) {
+  const dummy = new ListNode();
+  let head = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      head.next = list1;
+      list1 = list1.next;
+    } else {
+      head.next = list2;
+      list2 = list2.next;
+    }
+    head = head.next;
+  }
+  head.next = list1 || list2;
+  return dummy.next;
+};
---
+++ problems/0876链表的中间结点.js
@@ -0,0 +1,31 @@
+/*
+ * [876] 链表的中间结点
+ */
+
+/* 
+给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let fast = head;
+  let slow = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return slow;
+};

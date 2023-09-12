9960261692a1c67b35f876231ce2e9f95125cf61
keeko <787018661@qq.com>
2023-09-13 03:46:56 +0800

    0021合并两个有序链表

---
+++ problems/0021合并两个有序链表.js
@@ -0,0 +1,41 @@
+/*
+ * [21] 合并两个有序链表
+ */
+
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  const dummy = new ListNode();
+  let head = dummy;
+  while (list1 || list2) {
+    if (list1 && list2) {
+      if (list1.val < list2.val) {
+        head.next = list1;
+        list1 = list1.next;
+      } else {
+        head.next = list2;
+        list2 = list2.next;
+      }
+    } else if (list1) {
+      head.next = list1;
+      list1 = list1.next;
+    } else {
+      head.next = list2;
+      list2 = list2.next;
+    }
+    head = head.next;
+  }
+  return dummy.next;
+};

85550fdba749805753c77ccf0ec3aef40c2bd7c3
Keeko <787018661@qq.com>
2023-09-19 16:27:48 +0800

    0111二叉树的最小深度 v02

--- problems/0111二叉树的最小深度.js
+++ problems/0111二叉树的最小深度.js
@@ -22,7 +22,24 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
 var minDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left && !root.right) {
+    return 1;
+  }
+  if (!root.left) {
+    return minDepth(root.right) + 1;
+  }
+  if (!root.right) {
+    return minDepth(root.left) + 1;
+  }
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
+
+var minDepthPreVersion = function(root) {
   if (!root) {
     return 0;
   }

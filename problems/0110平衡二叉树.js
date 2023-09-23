b29e70268d93174db20eeda1244aa85872c36aba
keeko <787018661@qq.com>
2023-09-23 17:04:29 +0800

    0110平衡二叉树

--- problems/0110平衡二叉树.js
+++ problems/0110平衡二叉树.js
@@ -1,11 +1,9 @@
 /*
- * @lc app=leetcode.cn id=110 lang=javascript
- * @lcpr version=21909
- *
  * [110] 平衡二叉树
  */
 
-// @lc code=start
+// 平衡二叉树：左右子树的高度差不超过1
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -15,7 +13,6 @@
  * }
  */
 
-// 平衡二叉树：左右子树的高度差不超过1
 
 /**
  * @param {TreeNode} root
@@ -34,4 +31,22 @@ var isBalanced = function(root) {
     return Math.max(l, r) + 1;
   }
   return getHeight(root) < 10000;
+};
+
+// ↑ 优化解法
+
+// ↓ 自己的思路
+var isBalanced = function(root) {
+  const res = [];
+  function getDepth(node) {
+    if (!node) {
+      return 0;
+    }
+    const l = getDepth(node.left);
+    const r = getDepth(node.right);
+    res.push(Math.abs(l - r));
+    return Math.max(l, r) + 1;
+  }
+  getDepth(root);
+  return !res.some(item => item > 1);
 };

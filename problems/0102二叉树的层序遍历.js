f155af223b0edd29774a6bc1c1f9a81e8815bfe0
keeko <787018661@qq.com>
2023-09-24 22:22:13 +0800

    0102二叉树的层序遍历

--- problems/0102二叉树的层序遍历.js
+++ problems/0102二叉树的层序遍历.js
@@ -1,3 +1,46 @@
+/*
+ * [102] 二叉树的层序遍历
+ */
+/* 
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrder = function(root) {
+  let res = [];
+  if (!root) {
+    return res;
+  }
+
+  let queue = [root];
+
+  while (queue.length) {
+    const valArr = [];
+    const level = [];
+    for (let i = 0; i < queue.length; i++) {
+      const curNode = queue[i];
+      valArr.push(curNode.val);
+      curNode.left && level.push(curNode.left);
+      curNode.right && level.push(curNode.right);
+    }
+    res.push(valArr);
+    queue = level;
+  }
+  return res;
+};
+
 var levelOrder = function(root) {
   if (root === null) {
     return [];

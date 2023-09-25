6d9e9728aca59f2377785f39f653d85d48db42ce
Keeko <787018661@qq.com>
2023-09-25 20:34:30 +0800

    0637二叉树的层平均值

---
+++ problems/0637二叉树的层平均值.js
@@ -0,0 +1,48 @@
+/*
+ * [637] 二叉树的层平均值
+ */
+/* 
+  给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10-5 以内的答案可以被接受。
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+  const resArr = [];
+  if (!root) {
+    return res;
+  }
+  
+  let queue = [root];
+  while (queue.length) {
+    const level = [];
+    const valArr = [];
+    for (let node of queue) {
+      valArr.push(node.val);
+      node.left && level.push(node.left);
+      node.right && level.push(node.right);
+    }
+    resArr.push(getAvg(valArr));
+    queue = level;
+  }
+
+  function getAvg(arr) {
+    const count = arr.length;
+    let sum = 0;
+    for (let num of arr) {
+      sum += num;
+    }
+    return sum / count;
+  }
+
+  return resArr;
+};

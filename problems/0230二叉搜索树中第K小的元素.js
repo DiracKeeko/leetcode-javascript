56d3ce1a1f07b7cba83934c4d098e05fcb7a39ae
keeko <787018661@qq.com>
2023-10-01 06:57:11 +0800

    0230二叉搜索树中第K小的元素 递归

--- problems/0230二叉搜索树中第K小的元素.js
+++ problems/0230二叉搜索树中第K小的元素.js
@@ -1,21 +1,45 @@
+/*
+ * [230] 二叉搜索树中第K小的元素
+ */
+
 /* 
-二叉搜索树的性质:
-左子树的节点值都小于等于根节点值
-右子树的节点值都大于等于根节点值
+  给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
 */
 
+/* 
+  二叉搜索树的性质:
+  左子树的节点值都小于等于根节点值
+  右子树的节点值都大于等于根节点值
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+
 var kthSmallest = function(root, k) {
-  let num = 0;
   let res;
-  function inOrder(root) {
-    if (root === null) return;
-    inOrder(root.left);
-    num++;
-    if (num === k) {
-      res = root.val
+  let count = 0;
+  function inOrder(node) {
+    if (!node) {
       return;
     }
-    inOrder(root.right);
+    inOrder(node.left);
+    count += 1;
+    if (count === k) {
+      res = node.val;
+      return;
+    }
+    inOrder(node.right);
   }
   inOrder(root);
   return res;

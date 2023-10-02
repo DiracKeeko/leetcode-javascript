738bd104d80e490b7d240d1f5ba1b4333a955f15
keeko <787018661@qq.com>
2023-10-02 14:12:51 +0800

    0230二叉搜索树中第K小的元素_迭代实现

---
+++ problems/0230二叉搜索树中第K小的元素_迭代实现.js
@@ -0,0 +1,44 @@
+/*
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+/* 
+  给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+*/
+
+/* 
+  二叉搜索树的性质:
+  左子树的节点值都小于等于根节点值
+  右子树的节点值都大于等于根节点值
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+
+var kthSmallest = function(root, k) {
+  let count = 0;
+  const stack = [];
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    count += 1;
+    root = stack.pop();
+    if (count === k) {
+      return root.val;
+    } 
+    root = root.right;
+  }
+}

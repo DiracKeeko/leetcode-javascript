ca57131199d8e1f59105cff35edcd7a1273a0ff9
Keeko <787018661@qq.com>
2023-10-07 16:26:27 +0800

    0151反转字符串中的单词

---
+++ problems/0151反转字符串中的单词.js
@@ -0,0 +1,48 @@
+/*
+ * [151] 反转字符串中的单词
+ */
+
+/* 
+  给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+
+  单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+
+  返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+
+  注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+  const arr = [];
+  let word = "";
+  while (s[left] === " ") {
+    left += 1;
+  }
+  while (s[right] === " ") {
+    right -= 1;
+  }
+  while (left <= right) {
+    let ch = s[left];
+    if (ch !== " ") {
+      word += ch;
+    } else if (ch === " " && word !== "") {
+      arr.unshift(word);
+      word = "";
+    }
+    left += 1;
+  }
+  arr.unshift(word);
+  return arr.join(" ");
+};
+
+// ↓ 一个工程中的常规实现
+var reverseWords = function(s) {
+  const wordArr = s.trim().split(" ").filter(item => item !== "");
+  wordArr.reverse();
+  return wordArr.join(" ");
+};

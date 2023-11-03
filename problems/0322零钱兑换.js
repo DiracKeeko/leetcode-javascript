72fa672064711d6faf1008944822dbb8fc849a09
Keeko <787018661@qq.com>
2023-11-03 11:42:22 +0800

    0322零钱兑换 (注意初始化条件)

--- problems/0322零钱兑换.js
+++ problems/0322零钱兑换.js
@@ -9,21 +9,38 @@
 你可以认为每种硬币的数量是无限的。
 */
 
+/* 
+dp五步
+1. 确定dp数组（dp table）以及下标的含义
+  dp[j] 表示从coins [0, i]之间任取硬币, 凑齐j所需要的最小数量。
+
+2. 确定递推公式
+  dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+
+3. dp数组如何初始化
+  dp 全部填充Infinity, dp[0] = 0;
+
+4. 确定遍历顺序
+  两层for循环, 先容量 再物品
+
+5. 举例推导dp数组
+*/
 /**
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
 var coinChange = function (coins, amount) {
-  // dp[i] 代表金额i的最优解，即最小使用张数
-  const dp = new Array(amount + 1).fill(Infinity);
+  const dp = Array(amount + 1).fill(Infinity);
   dp[0] = 0;
-  for (let i = 0; i <= amount; i++) {
+
+  for (let j = 1; j <= amount; j++) {
     for (let coin of coins) {
-      if (i - coin >= 0) {
-        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      if (j >= coin) {
+        dp[j] = Math.min(dp[j], dp[j - coin] + 1);
       }
     }
   }
+
   return dp[amount] === Infinity ? -1 : dp[amount];
 };

e8ae134c7d47f84aee6d6e37a274407078380bca
Keeko <787018661@qq.com>
2023-11-18 15:51:25 +0800

    0004寻找两个正序数组的中位数 v2 O( (m+n)/2 )

--- problems/0004寻找两个正序数组的中位数.js
+++ problems/0004寻找两个正序数组的中位数.js
@@ -21,3 +21,46 @@ var findMedianSortedArrays = function(nums1, nums2) {
     return (arr[mid] + arr[mid - 1]) / 2;
   }
 };
+
+// v2 不考虑时间复杂度问题，用双指针来实现 O((m + n) / 2)
+// 每次都向新数组尾部插入两个输入数组最小的值
+var findMedianSortedArrays = function(nums1, nums2) {
+  const arr = [];
+  const [len1, len2] = [nums1.length, nums2.length];
+  const mid = (len1 + len2) >> 1;
+  let n1 = 0;
+  let n2 = 0;
+  while (n1 < len1 || n2 < len2) {
+    if (n1 === len1) {
+      arr.push(nums2[n2]);
+      n2 += 1;
+    } else if (n2 === len2) {
+      arr.push(nums1[n1]);
+      n1 += 1;
+    } else {
+      const min1 = nums1[n1];
+      const min2 = nums2[n2];
+      if (min1 < min2) {
+        arr.push(min1);
+        n1 += 1;
+      } else {
+        arr.push(min2);
+        n2 += 1;
+      }
+    }
+
+    if (arr.length > mid) {
+      if ((len1 + len2) % 2 === 1) {
+        return arr[mid];
+      } else {
+        return (arr[mid] + arr[mid - 1]) / 2;
+      }
+    }
+  }
+};
+/* 
+v2
+2094/2094 cases passed (92 ms)
+Your runtime beats 84.97 % of javascript submissions
+Your memory usage beats 93.86 % of javascript submissions (44.9 MB)
+*/

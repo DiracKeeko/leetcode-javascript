17a868170b8de2556c5bff9829df7f1f2771b749
Keeko <787018661@qq.com>
2023-11-20 14:58:26 +0800

    0383赎金信 v2 arr存储

--- problems/0383赎金信.js
+++ problems/0383赎金信.js
@@ -23,11 +23,11 @@
  * @param {string} magazine
  * @return {boolean}
  */
+
+// v1 用map记录
 var canConstruct = function(ransomNote, magazine) {
   const ransomNoteMap = getCharMap(ransomNote);
   const magazineMap = getCharMap(magazine);
-  console.log("ransomNoteMap->", ransomNoteMap);
-  console.log("magazineMap->", magazineMap);
   for (const key in ransomNoteMap) {
     if (ransomNoteMap[key] > (magazineMap[key] || 0)) {
       return false;
@@ -44,6 +44,27 @@ var canConstruct = function(ransomNote, magazine) {
     return obj;
   }
 };
+/* 
+v1
+128/128 cases passed (84 ms)
+Your runtime beats 53.05 % of javascript submissions
+Your memory usage beats 48.81 % of javascript submissions (43.7 MB)
+*/
+
+// v2 用arr记录 这种情况array的存储优于map存储
+var canConstruct = function(ransomNote, magazine) {
+  const arr = Array(26).fill(0);
+  const base = "a".charCodeAt(0);
+  magazine.split("").forEach(ch => arr[ch.charCodeAt(0) - base]++);
+  ransomNote.split("").forEach(ch => arr[ch.charCodeAt(0) - base]--);
+  return arr.every(item => item >= 0);
+};
+/* 
+v2
+128/128 cases passed (60 ms)
+Your runtime beats 98.69 % of javascript submissions
+Your memory usage beats 21.15 % of javascript submissions (45 MB)
+*/
 
 const ransomNote = "a", magazine = "b";
 const res = canConstruct(ransomNote, magazine);

95e21182dbd62963b60fee783ecf4a6d1af2cc55
keeko <787018661@qq.com>
2023-12-26 21:23:11 +0800

    策略模式 04适配器模式 +1

--- 05设计模式/04适配器模式.js
+++ 05设计模式/04适配器模式.js
@@ -56,3 +56,50 @@ class Adapter3 {
 
 const adapter1 = new Adapter1(data1);
 const formattedData = adapter1.getTargetData();
+
+
+
+// 上面的例子用策略模式也能实现。 再来一个例子
+// 温度转换器，将摄氏度转换为华氏度。
+
+// 以摄氏度为单位的接口
+class CelsiusTemperature {
+  constructor(value) {
+    this.value = value;
+  }
+
+  getCelsius() {
+    return this.value;
+  }
+}
+
+// 以华氏度为单位的接口
+class FahrenheitTemperature {
+  constructor(value) {
+    this.value = value;
+  }
+
+  getFahrenheit() {
+    return this.value;
+  }
+}
+
+// 适配器
+class TemperatureAdapter {
+  constructor(celsiusTemperature) {
+    this.celsiusTemperature = celsiusTemperature;
+  }
+
+  getFahrenheit() {
+    const celsius = this.celsiusTemperature.getCelsius();
+    const fahrenheit = (celsius * 9) / 5 + 32;
+    return fahrenheit;
+  }
+}
+
+// 测试适配器
+const celsiusTemp = new CelsiusTemperature(30); // 摄氏度为 30 度
+const adapter = new TemperatureAdapter(celsiusTemp); // 使用适配器将摄氏度转换为华氏度
+
+const fahrenheit = adapter.getFahrenheit(); // 获取华氏度
+console.log(`华氏温度：${fahrenheit.toFixed(2)}°F`); // 输出：华氏温度：86.00°F

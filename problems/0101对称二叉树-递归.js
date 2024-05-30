ac375f4c261b48bc62d63008fe92a1ef8a71a606
Keeko <787018661@qq.com>
2024-05-30 17:30:19 +0800

    二叉树 review

--- problems/0101对称二叉树-递归.js
+++ problems/0101对称二叉树-递归.js
@@ -4,6 +4,28 @@
  * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
  */
 
+// v final
+var isSymmetric = function(root) {
+  if (!root) {
+    return true;
+  }
+
+  function check(a, b) {
+    if (a === null && b === null) {
+      return true;
+    }
+    if (a === null || b === null) {
+      return false;
+    }
+    if (a.val !== b.val) {
+      return false;
+    }
+    return check(a.left, b.right) && check(a.right, b.left);
+  }
+
+  return check(root.left, root.right);
+};
+
 var isSymmetric = function(root) {
   function check(a, b) {
     if (a === null && b === null) {

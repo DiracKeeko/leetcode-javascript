f3d02c7a75fb6ffc777006b57e3361b15cf49e51
Keeko <787018661@qq.com>
2024-09-11 11:41:11 +0800

    dp 0121 买卖股票01

--- problems/0121买卖股票的最佳时机.js
+++ problems/0121买卖股票的最佳时机.js
@@ -40,15 +40,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+  const len = prices.length;
   const dp = [[0, -prices[0]]];
 
-  for (let i = 1; i < prices.length; i++) {
-    dp[i] = [];
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-    dp[i][1] = Math.max(dp[i - 1][1], - prices[i]);
+  for (let i = 1; i < len; i++) {
+    const withoutStock = Math.max(dp[i - 1][0], prices[i] + dp[i - 1][1]);
+    const withStock = Math.max(dp[i - 1][1], -prices[i]);
+    dp[i] = [withoutStock, withStock];
   }
-  console.table(dp);
-  return dp[prices.length - 1][0];
+  
+  return dp[len - 1][0];
 };
 
 const prices = [7,1,5,3,6,4];

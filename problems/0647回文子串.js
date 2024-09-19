41c048e269bdf647e38e6102d5775fbeaae2c631
Keeko <787018661@qq.com>
2024-09-19 15:32:22 +0800

    dp 0647 回文子串

--- problems/0647回文子串.js
+++ problems/0647回文子串.js
@@ -54,9 +54,11 @@ dp五步
  * @param {string} s
  * @return {number}
  */
+
+// v1
 var countSubstrings = function(s) {
   const l = s.length;
-  const dp = Array(l).fill(0).map(() => (Array(l).fill(false)));
+  const dp = Array(l).fill(0).map(() => (Array(l).fill(false))); // ## 全量初始化逻辑
   let res = 0;
   
   for (let i = l - 1; i >= 0; i--) {
@@ -66,7 +68,7 @@ var countSubstrings = function(s) {
         if (j - i <= 1) {
           dp[i][j] = true;
           res += 1;
-        } else if (dp[i + 1][j - 1]) {
+        } else if (dp[i + 1][j - 1]) { // 这里也可以学习
           dp[i][j] = true;
           res += 1;
         }
@@ -78,6 +80,129 @@ var countSubstrings = function(s) {
   return res;
 };
 
+// v2
+var countSubstrings = function(s) {
+  const len = s.length;
+  const dp = [];
+  let res = 0;
+
+  for (let i = len - 1; i >= 0; i--) {
+    dp[i] = Array(len).fill(false);
+    for (let j = i; j < len; j++) {
+      if (s[i] === s[j]) {
+        if (j - i <= 1) {
+          dp[i][j] = true;
+          res += 1;
+        } else {
+          dp[i][j] = dp[i + 1][j - 1];
+          dp[i][j] && (res += 1);
+        }
+      }
+    }
+  }
+  return res;
+};
+
 const s = "fdsklf";
 const res = countSubstrings(s);
 console.log("res->", res);
+
+/* 
+分析: 
+对 str = "abba";
+
+let dp;
+
+// init
+dp = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false]
+]
+
+// i = 3; j = 3; -> s[3] == s[3] -> j - i == 0 -> dp[3][3] = true
+dp = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, false, true]
+]
+
+
+// i = 2; j = 2; -> s[2] == s[2] -> j - i == 0 -> dp[2][2] = true
+dp = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 2; j = 3; -> s[2] != s[3] -> 不进入if -> dp[2][3]保持默认值false
+dp = [
+  [false, false, false, false],
+  [false, false, false, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 1; j = 1; -> s[1] == s[1] -> j - i == 0 -> s[1][1] = true
+dp = [
+  [false, false, false, false],
+  [false, true, false, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 1; j = 2; -> s[1] == s[2] -> j - i == 1 -> dp[1][2] = true
+dp = [
+  [false, false, false, false],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 1; j = 3; -> s[1] != s[3] -> 不进入if -> dp[1][3]保持默认值false
+dp = [
+  [false, false, false, false],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 0; j = 0; -> s[0] == s[0] -> j - i == 0 -> dp[0][0] = true
+dp = [
+  [true, false, false, false],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 0; j = 1; -> s[0] != s[1] -> 不进入if -> dp[0][1]保持默认值false
+dp = [
+  [true, false, false, false],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 0; j = 2; -> s[0] != s[2] -> 不进入if -> dp[0][2]保持默认值false
+dp = [
+  [true, false, false, false],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+// i = 0; j = 3; -> s[0] == s[3] -> j - i == 2 -> dp[0][3] = dp[1][2] = true;
+dp = [
+  [true, false, false, true],
+  [false, true, true, false],
+  [false, false, true, false],
+  [false, false, false, true]
+]
+
+实际有意义的半区在右上部分
+(dp[i][j] 表示 [i, j] 之间是否是回文串， dp[2][3]的含义与dp[3][2]相同，不可以重复计算)
+
+*/

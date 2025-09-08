5fc9735c3fe4e96274b59e75beb7790cdfb1e0dc
Keeko <787018661@qq.com>
2025-09-08 20:47:57 +0800

    2025年9月8日20:47:55

---
+++ problems/0010正则表达式匹配.js
@@ -0,0 +1,87 @@
+/* 
+0010 正则表达式匹配
+
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖 整个 字符串 s 的，而不是部分字符串。
+
+*/
+/* 
+1.确定dp数组的含义
+dp[i][j] 表示字符串s的前i个字符，和字符串p的前j个字符能否完全匹配。
+(子串 s[0...i-1] 是否匹配 p 的子串 p[0...j-1])
+
+dp 表格的大小为 (s.length + 1) x (p.length + 1)，多出来的一行一列是为了方便处理空字符串的情况。
+
+2.确定递推公式
+case1 
+当p[j - 1] !== "*" 时
+dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] === p[j - 1] || p[j - 1] === ".")
+
+p[j - 1]不为通配符，则dp[i][j] 取决于 dp[i - 1][j - 1]以及 p[j - 1]是否和 s[i - 1]匹配
+p[j - 1] 和 s[i - 1] 匹配意味着 s[i - 1] === p[j - 1] || p[j - 1] === "."
+
+case2
+当p[j - 1] === "*" 时
+又分为匹配0次和匹配多次
+dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] === p[j - 2] || p[j - 2] === "."))
+
+case2.1 匹配0次
+dp[i][j] === dp[i][j - 2]
+
+case2.2 匹配1次或多次
+case2.2.1匹配1次
+dp[i][j] === dp[i - 1][j] && s[i - 1] === p[j - 2]
+
+case2.2.2匹配多次
+dp[i][j] === dp[i - 1][j] && p[j - 2] === "."
+
+case
+
+
+dp 表格的大小为 (s.length + 1) x (p.length + 1)，多出来的一行一列是为了方便处理空字符串的情况。
+
+*/
+
+var isMatch = function(s, p) {
+  const m = s.length;
+  const n = p.length;
+  
+  const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
+
+  dp[0][0] = true;
+  for (let j = 1; j <= n; j++) {
+    if (p[j - 1] === "*") {
+      dp[0][j] = dp[0][j - 2];
+    }
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      const sChar = s[i - 1];
+      const pChar = p[j - 1];
+
+      if (pChar !== "*") {
+        // dp[i][j] = dp[i - 1][j - 1] && (sChar === pChar || pChar === ".");
+        // ↓ 改成下面的写法
+        if (sChar === pChar || pChar === ".") {
+          dp[i][j] = dp[i - 1][j - 1];
+        }
+      } else {
+        // pChar === "*"
+        const pPrevChar = p[j - 2]; // "*"前面的字符
+        
+        const matchZero = dp[i][j - 2];
+        let matchOneOrMore = false;
+        if (sChar === pPrevChar || pPrevChar === ".") {
+          matchOneOrMore = dp[i - 1][j];
+        }
+
+        dp[i][j] = matchZero | matchOneOrMore;
+      }
+    }
+  }
+  return dp[m][n];
+};

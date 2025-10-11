63b89737e38005a456c2ad27abe3a5a7b2b55c02
Keeko <787018661@qq.com>
2025-10-11 17:08:37 +0800

    2025年10月11日17:08:36

--- problems/0015三数之和.js
+++ problems/0015三数之和.js
@@ -18,6 +18,7 @@
  * @return {number[][]}
  */
 
+// v1
 var threeSum = function (nums) {
   if (nums.length < 3) {
     return [];
@@ -54,3 +55,42 @@ var threeSum = function (nums) {
   }
   return res;
 };
+
+
+// v2 self 逻辑更清晰
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const len = nums.length;
+  if (nums[0] > 0 || len < 3) {
+    return [];
+  }
+
+  const res = [];
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let j = i + 1;
+    let k = len - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        res.push([nums[i], nums[j], nums[k]]);
+        j += 1; // 从这一行开始，到最近的代码块结束，和v1版本不同
+        k -= 1;
+        while (nums[j] === nums[j - 1]) {
+          j += 1;
+        }
+        while (nums[k] === nums[k + 1]) {
+          k -= 1;
+        }
+      } else if (sum > 0) {
+        k -= 1;
+      } else {
+        // sum < 0
+        j += 1;
+      }
+    }
+  }
+  return res;
+}
